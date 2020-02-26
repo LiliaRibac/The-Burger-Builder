51d26d371697a8fd50cff90a1ec31d5ae1e83903
LiliaRibac <ribac.lilia@gmai.com>
2020-02-26 14:21:40 -0700

    implementing the Backdrop Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    purchasing:false
   };
 
   updatePurchasesState(ingredients) {
@@ -68,6 +69,18 @@ class BurgerBuilder extends Component {
     this.updatePurchasesState(updatedIngredients);
   };
 
+  purchaseHandler =()=> {
+    this.setState ({purchasing:true})
+  }
+
+  purchaseCancelHandler =() =>{
+    this.setState({purchasing:false})
+  }
+
+  purchesContinueHandler =()=>{
+    alert('You continue!')
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -78,8 +91,12 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-      <Modal>
-        <OrderSummary ingredients={this.state.ingredients}/>
+      <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+        <OrderSummary ingredients={this.state.ingredients}
+        price={this.state.totalPrice}
+          purchaseCancelled={this.purchaseCancelHandler}
+          purchaseContinued={this.purchesContinueHandler}
+        />
       </Modal>
     
        
@@ -89,6 +106,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           purchasable={this.state.purchasable}
+          ordered={this.purchaseHandler}
           price={this.state.totalPrice}
         />
       </Aux>
