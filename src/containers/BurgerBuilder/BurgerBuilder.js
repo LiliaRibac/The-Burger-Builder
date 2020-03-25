39e269276f3aa71d621d29a89e2dc147cbbe77b7
LiliaRibac <ribac.lilia@gmai.com>
2020-03-25 15:04:42 -0700

    Navigating to the Contact Data  Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,6 +24,8 @@ class BurgerBuilder extends Component {
     loading: false
   };
   componentDidMount() {
+    console.log(this.props);
+
     axios
       .get('https://react-my-burger-b2644.firebaseio.com/ingredients.json')
       .then(response => {
@@ -83,25 +85,36 @@ class BurgerBuilder extends Component {
 
   purchesContinueHandler = () => {
     // alert('You continue!')
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Lilia Ribac',
-        address: {
-          street: 'Pacii 1',
-          zipCode: '45632',
-          country: 'Arizona'
-        },
-        email: 'test@test.com'
-      },
-      deliverMethod: 'fastest'
-    };
-    axios
-      .post('/orders.json', order)
-      .then(response => this.setState({ loading: false, purchasing: false }))
-      .catch(error => this.setState({ loading: false, purchasing: false }));
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Lilia Ribac',
+    //     address: {
+    //       street: 'Pacii 1',
+    //       zipCode: '45632',
+    //       country: 'Arizona'
+    //     },
+    //     email: 'test@test.com'
+    //   },
+    //   deliverMethod: 'fastest'
+    // };
+    // axios
+    //   .post('/orders.json', order)
+    //   .then(response => this.setState({ loading: false, purchasing: false }))
+    //   .catch(error => this.setState({ loading: false, purchasing: false }));
+
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+    }
+    const queryString = queryParams.join('&');
+
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
   };
 
   render() {
